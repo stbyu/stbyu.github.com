ca05fab112e7109d9fa7b7c7dd68f96632c19089
雨宮千夏 <hanzexu990323@gmail.com>
2016-03-05 12:48:24 +0800

    update

--- lib/config.php
+++ lib/config.php
@@ -8,3 +8,14 @@
  */
 //定义网站标题
 $title = "Identity by HTML5 UP";
+
+//设置头像
+//请将头像文件放入 images 文件夹
+$avatar_filename = "avatar.jpg";
+
+//设置主标题
+$main_title = "JANE DOE";
+
+//设置二级标题
+$sec_title = "SENIOR PSYCHONAUTICS ENGINEER";
+
