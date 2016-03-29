76e347fa36081e0729983b75ba1fbdc5effd1f8b
雨宮千夏 <hanzexu990323@gmail.com>
2016-03-29 15:57:00 +0800

    update

--- index.php
+++ index.php
@@ -80,6 +80,7 @@ include_once 'lib/config.php';
     <footer id="footer">
         <ul class="copyright">
             <li>&copy; <?php echo $copyright; ?></li>
+            <li>Design: <a href="https://github.com/hanzexu990323/uuz-h5theme">UUZ-H5THEME
             <li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
         </ul>
     </footer>
