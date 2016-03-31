020881d6630888610c83446553e8cb1ef9273ff3
雨宮千夏 <hanzexu990323@gmail.com>
2016-03-31 14:58:25 +0800

    rename

---
+++ lib/config-simple.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * uuz-h5theme配置文件
+ * Created by PhpStorm.
+ * User: Amamiya Chinatsu's
+ * Date: 2016/3/5
+ * Time: 12:22
+ */
+//定义网站标题
+$title = "Identity by HTML5 UP";
+
+//设置头像
+//请将头像文件放入 images 文件夹
+$avatar_filename = "avatar.jpg";
+
+//设置主标题
+$main_title = "JANE DOE";
+
+//设置二级标题
+$sec_title = "SENIOR PSYCHONAUTICS ENGINEER";
+
+//设置连接地址
+//设置Twitter链接
+$url_twitter = "#";
+
+//设置Instagram链接
+$url_instagram = "#";
+
+//设置Facebook链接
+$url_facebook = "#";
+
+//设置版权信息
+$copyright = "Jane Doe";
+
